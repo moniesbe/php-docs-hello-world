08a9480cd20858bbdf4e66f6cbb60cf91e1a63df
moniesbe <monica.esteve.benet@gmail.com>
2025-06-21 10:21:51 +0200

    Update prueba_conexion.php

--- prueba_conexion.php
+++ prueba_conexion.php
@@ -1,7 +1,7 @@
 <?php
 // Recuperar variables de entorno
 $dbHost = getenv('DB_HOST');
-$dbName = "prueba";         
+$dbName = getenv('DB_NAME');         
 $dbUser = getenv('DB_USER');
 $dbPass = getenv('DB_PASSWORD');
 
